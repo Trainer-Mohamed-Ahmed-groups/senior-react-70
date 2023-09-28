01fc68c2bf40d821f2366cc42bfa29d9f834455d
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2023-09-28 13:09:33 +0300

    To Writing Markup with JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
+import "normalize.css/normalize.css"
 import logo from './logo.svg';
 import './App.css';
+import First, { Second } from "./components/First";
 
 function App() {
+  var courseName = "React course";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {courseName}
+      <First />
+      <Second />
+      {/* <img src={logo} alt="logo" /> */}
     </div>
   );
 }
---
+++ src/components/First.jsx
@@ -0,0 +1,16 @@
+function First() {
+    return (
+        <>
+            <h2>First</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem consequatur delectus odio, porro corrupti cupiditate tempore reprehenderit, distinctio cum eum blanditiis eligendi ex deleniti quibusdam quae vero, saepe explicabo illum?</p>
+        </>
+    )
+}
+
+export function Second() {
+    return (
+        <div>This is second</div>
+    )
+}
+
+export default First
